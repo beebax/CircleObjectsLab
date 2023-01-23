7b356ec5b62f2901dd05985c5f277fa5d5c490d9
belle <belle.baxley@gmail.com>
2023-01-23 16:52:33 -0500

    first commit

---
+++ Lab9 Circle Objects/Circle.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace Lab9_Circle_Objects
+{
+    public class Circle
+    {
+        //properties
+        public double Radius { get; set; }
+
+        //constructor
+        public Circle(double _radius)
+        {
+            Radius = _radius;
+        }
+
+        //default
+        public Circle()
+        {
+            Radius = 0;
+        }
+
+        //methods
+        public double CalculateCircumfrence()
+        {
+            double circumfrence = 2 * Math.PI * Radius;
+            return circumfrence;
+        }
+
+        public string CalculateFormattedCircumfrence()
+        {
+            return FormatNumber(CalculateCircumfrence());
+        }
+        public double CalculateArea()
+        {
+            double area = Radius * Radius * Math.PI;
+            return area;
+        }
+        public string CalculateFormattedArea()
+        {
+            return FormatNumber(CalculateArea());
+        }
+        private string FormatNumber(double x)
+        {
+            string formattedArea = Math.Round(x, 2).ToString();
+            return formattedArea;
+        }
+    }
+}
+
+
+
+
+       
---
+++ Lab9 Circle Objects/Program.cs
@@ -0,0 +1,42 @@
+﻿
+using Lab9_Circle_Objects;
+
+int circleCounter = 0;
+bool doingMath = true;
+while (doingMath)
+{
+    Console.WriteLine("Welcome to the Circle Tester");
+
+    //get input
+    Console.WriteLine("Enter radius: ");
+    double _radius = double.Parse(Console.ReadLine());
+
+    //make new circle
+    Circle myCircle = new Circle(_radius);
+
+    Console.WriteLine($"Circumfrence: {myCircle.CalculateFormattedCircumfrence()}");
+    Console.WriteLine($"Area: {myCircle.CalculateFormattedArea()}");
+    circleCounter++;
+
+    while (true)
+    {
+        Console.WriteLine("Do you want to go again? y/n");
+        string choice = Console.ReadLine().ToLower().Trim();
+        if (choice == "y")
+        {
+            doingMath = true;
+            break;
+        }
+        else if (choice == "n")
+        {
+            doingMath = false;
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Invalid input");
+        }
+    }
+}
+
+Console.WriteLine($"You made {circleCounter} circles. Goodbye");
